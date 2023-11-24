2b8940c81eec9ab639a9d73875b900a8a880d9f1
vaibhavkushwaha <vaibhavkushwaha1973@gmail.com>
2023-11-24 22:56:20 +0530

    project

--- isesa_app/home.py
+++ isesa_app/home.py
@@ -9,13 +9,17 @@ st.set_page_config(layout="wide",
                    page_title="Indian School Education Statistics Analysis",
                    page_icon='📚')
 
-def load_data(path):
-    df = pd.read_csv('Indian-school-education-statistics-analysis\isesa_app\gross-enrollment-ratio-2013-2016.csv') #loading the dataset to panda dataframe
+
+@st.cache_data()
+
+
+def load_data():
+    df = pd.read_csv('gross-enrollment-ratio-2013-2016.csv') #loading the dataset to panda dataframe
     st.sidebar.success('Dataset Loaded Successfully')
     return df
 
 with st.spinner("Processing Immigration data...."):
-    df=load_data('C:\Users\pc\OneDrive\Desktop\mini project\Indian-school-education-statistics-analysis\isesa_app\gross-enrollment-ratio-2013-2016.csv')
+    df=load_data()
 
 
 
